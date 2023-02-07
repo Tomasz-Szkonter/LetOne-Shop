3652003465511cf6d8e480024bf841385eff181a
Tomasz Szkonter <Tomasz.Szkonter@gmail.com>
2023-02-07 05:34:41 +0100

    Establishing bootstrap styling system

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Container>header</Container>
+    </header>
+  );
+};
+
+export default Header;
